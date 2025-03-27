d5ea670a1edfa60245009f17ac26c1bf8f6bb9b4
Joe Letner <jrletner@gmail.com>
2025-03-26 19:46:31 -0500

    Implement task action confirmation dialog and enhance task service notifications

---
+++ src/app/shared/dialog-animations-example-dialog/dialog-animations-example-dialog.component.ts
@@ -0,0 +1,47 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { TaskService } from '../task.service';
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.component.html',
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAnimationsExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogAnimationsExampleDialog>);
+  constructor(private taskService: TaskService) {}
+
+  get action() {
+    console.log('Action:', this.taskService.taskAction());
+
+    return this.taskService.taskAction();
+  }
+
+  handleCancel() {
+    this.taskService.taskToAction.set(null);
+  }
+
+  handleAction(action: string) {
+    const taskID = this.taskService.taskToAction();
+    if (action === 'deleteTask') {
+      this.taskService.deleteTask(taskID!);
+    } else if (action === 'completeTask') {
+      this.taskService.completeTask(taskID!);
+    }
+  }
+}
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,35 +1,51 @@
-import { Component, Inject } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { TaskService } from '../shared/task.service';
 import { SharedModule } from '../shared/shared.module';
 import { NotificationService } from '../shared/notification.service';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { DialogAnimationsExampleDialog } from '../shared/dialog-animations-example-dialog/dialog-animations-example-dialog.component';
 
 @Component({
   selector: 'app-task-list',
-  imports: [SharedModule],
+  imports: [SharedModule, MatButtonModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
 })
 export class TaskListComponent {
-  constructor(private taskService: TaskService, private notify: NotificationService) {}
+  constructor(
+    private taskService: TaskService,
+    private notify: NotificationService
+  ) {}
 
   // method to get tasks
   get tasks() {
     return this.taskService.tasks();
   }
 
-  // method for deleting task
-  deleteTask(id: number) {
-    this.taskService.deleteTask(id);
-    this.notify.show('✌️ Task Deleted!');
+  // MATERIAL
+  readonly dialog = inject(MatDialog);
+
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string,
+    id: number,
+    action: string
+  ): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+    this.taskService.taskAction.set(action);
+    this.taskService.taskToAction.set(id);
   }
-
-  // method to complete task
-  completeTask(id: number) {
-    this.taskService.completeTask(id);
-    this.notify.show('✔️ Task Completed!');
-    }
-  }
-
-
-
+}
