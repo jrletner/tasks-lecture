ab354f5dcfdea182d3f8ac7a4bea23a670f2eddc
Joe Letner <jrletner@gmail.com>
2025-03-26 19:02:14 -0500

    Update Angular dependencies and enhance task form styles with Material badges

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -2,15 +2,21 @@ import { Component } from '@angular/core';
 import { TaskService } from '../shared/task.service';
 import { SharedModule } from '../shared/shared.module';
 import { NotificationService } from '../shared/notification.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-task-form',
-  imports: [SharedModule],
+  imports: [SharedModule, MatBadgeModule, MatButtonModule, MatIconModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css',
 })
 export class TaskFormComponent {
-  constructor(private taskService: TaskService, private notify: NotificationService) {}
+  constructor(
+    private taskService: TaskService,
+    private notify: NotificationService
+  ) {}
 
   // Properties
   newTaskTitle = '';
@@ -31,7 +37,6 @@ export class TaskFormComponent {
       this.taskService.addTask(this.newTaskTitle);
       this.newTaskTitle = '';
     }
-      this.notify.show('✅ Task added!');
-    }
+    this.notify.show('✅ Task added!');
+  }
 }
-
